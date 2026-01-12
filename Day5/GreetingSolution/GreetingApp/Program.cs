3d220f74942f0781cc698025e2a8c28625e037d8
s-hritik <hritiksingh158@gmail.com>
2026-01-12 20:03:36 +0530

    LibClass

---
+++ Day5/GreetingSolution/GreetingApp/Program.cs
@@ -0,0 +1,43 @@
+﻿//using GreetingLibrary;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace GreetingApp
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string name;
+//            Console.WriteLine("Please, enter your name sir/mam: ");
+//            name = Console.ReadLine();
+//            Console.WriteLine(GreetingHelper.GetGreeting(name));
+
+//        }
+//    }
+//}
+
+
+using System;
+using GreetingLibrary;
+
+namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+
+            string greeting = GreetingHelper.GetGreeting(name);
+
+            Console.WriteLine(greeting);
+
+            Console.ReadLine(); // keeps console open
+        }
+    }
+}
---
+++ Day5/GreetingSolution/GreetingLibrary/GreetingHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GreetingLibrary
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return "Hello, Guest!";
+            }
+
+            return $"Hello, {name}!";
+        }
+    }
+}
